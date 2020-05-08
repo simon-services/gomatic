d1f3523e2bf77f15d6518960b927e94c2c0928ae
opunix <francisc.simon@evalgo.org>
2020-05-08 11:07:47 +0200

    moved most of the logic from init.sh to the Makefile

--- Makefile
+++ Makefile
@@ -1,6 +1,31 @@
 .PHONY: all
 all: init
 
+os-init:
+	apt update
+	apt install -y gnupg2 curl procps git unzip make python3 python3-pip
+	echo "deb https://download.gocd.org /" | tee /etc/apt/sources.list.d/gocd.list
+	curl https://download.gocd.org/GOCD-GPG-KEY.asc | apt-key add -
+	apt update
+
+gocd-server:
+	pip3 install gomatic
+	apt install -y go-server
+	systemctl enable go-server
+	systemctl start go-server
+
+gocd-agent: preconfigure-gocd-agent start-gocd-agent
+	apt install -y go-agent
+	mkdir -pv /var/lib/go-agent/config/
+	chown -Rf go:go /var/lib/go-agent
+	@echo "waiting 60 sec for the server to start..."
+	sleep 60
+	python3 init.py
+	systemctl enable go-agent
+	systemctl start go-agent
+
+gocd: os-init gocd-server gocd-agent
+
 init: reload init-lxd-server
 
 reload:
--- init.sh
+++ init.sh
@@ -1,37 +1,9 @@
 #!/bin/bash
 
-apt update
-# install required pkgs
-apt install -y gnupg2 curl procps git unzip make python3 python3-pip
-
-# install gomatic
-pip3 install gomatic
-# enable the gocd source.list/pkgs
-echo "deb https://download.gocd.org /" | tee /etc/apt/sources.list.d/gocd.list
-curl https://download.gocd.org/GOCD-GPG-KEY.asc | apt-key add -
-apt update
-# install and intialize the goserver
-apt install -y go-server
-systemctl enable go-server
-# just give the go server more time to start
-systemctl start go-server
-# install the required go agent to run the first 2 pipelines
-apt install -y go-agent
-# get the gomatic repository with the pipelines
+# get the repository from github
 git clone https://github.com/simon-services/gomatic.git
-
-echo "waiting 60 sec for the go server to start..."
-sleep 60
-
 cd gomatic
-# create the required go-agent config folder in the default home folder
-mkdir -pv /var/lib/go-agent/config/
-# change owner so that the agent is able to use the folder
-chown -Rf go:go /var/lib/go-agent
-# autoregister the go-agent
-python3 init.py
-# initialize the pipelines
-make
-# start the agent as last ... so that all configurations made to the server config are finished
-systemctl enable go-agent
-systemctl start go-agent
+# initialize gocd system
+make gocd
+# import gomatic pipelines into gocd
+make init
