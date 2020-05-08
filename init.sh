1002cde99348c296c3a5992c7ed76d267c176152
Francisc Simon <francisc.simon@evalgo.org>
2020-05-08 11:18:08 +0200

    updated init script

--- init.sh
+++ init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# make sure you can get the code and run the makefile
+apt update
+apt install -y make git
+
 # get the repository from github
 git clone https://github.com/simon-services/gomatic.git
 cd gomatic
